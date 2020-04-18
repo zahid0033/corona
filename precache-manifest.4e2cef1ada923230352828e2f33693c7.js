ced64ec06f0dc8fc14b7193313aada034ceb3f2b
zahid0033 <zahidhossain0033@gmail.com>
2020-04-18 12:06:54 +0600

    Updates

---
+++ precache-manifest.4e2cef1ada923230352828e2f33693c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03ad733bed5ed1716a14d5049d288b7",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "30b72fee06fe7458ce9a",
+    "url": "/corona/static/css/main.b0c3af57.chunk.css"
+  },
+  {
+    "revision": "df648c3624d739eacdc1",
+    "url": "/corona/static/js/2.1a9e8100.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona/static/js/2.1a9e8100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b72fee06fe7458ce9a",
+    "url": "/corona/static/js/main.3ff3ad80.chunk.js"
+  },
+  {
+    "revision": "10563266cc97041c2017",
+    "url": "/corona/static/js/runtime-main.b4d4afbe.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona/static/media/image.d7265326.png"
+  }
+]);
